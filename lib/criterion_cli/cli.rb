b817f9d78b3ade529f6a96b7173e68e69775cb8f
Ethan Sagin <ethansagin@gmail.com>
2019-05-15 16:00:42 +0000

    cast list work

--- lib/criterion_cli/cli.rb
+++ lib/criterion_cli/cli.rb
@@ -10,7 +10,18 @@ class CriterionCli::Cli
   html = open("https://www.criterion.com/films/1333-2-or-3-things-i-know-about-her")
   doc = Nokogiri::HTML(html)
   duration = doc.css("ul.film-meta-list li")[3].text
-  
+  color = doc.css("ul.film-meta-list li")[4].text
+  language = doc.css("ul.film-meta-list li")[6].text.strip
+  cast_k = []
+  cast_v = []
+  doc.css("dl.creditList")[0].css("dt span").each do |x|
+    cast_k << x.text
+  end
+  doc.css("dl.creditList")[0].css("dd span").each do |y|
+    y
+        binding.pry
+
+  end
   
 
   #document.querySelectorAll("ul.film-meta-list")[0].querySelectorAll("li meta")[0].attributes[1].textContent
