47bab6381eaf2eec83722f518045ce225c75a8aa
Ethan Sagin <ethansagin@gmail.com>
2019-05-15 22:32:38 +0000

    cli work

--- lib/criterion_cli/cli.rb
+++ lib/criterion_cli/cli.rb
@@ -5,50 +5,48 @@ require 'nokogiri'
 class CriterionCli::Cli
   
   def call
-  Scraper.movies_scraper
-  puts "Welcome"
-  puts "1) List all Criterion Directors."
-  puts "2) List all Criterion Films."
-  input = nil
-  while input =! "exit"
-    input = gets.strip.downcase
-    case input
+    Scraper.movies_scraper
+    # print movie profile
+    # show what films director has made
+    # show who director has collaborated with
+    # list all directors
+    # list all movies
+    puts "Welcome to Criterion Collection List"
+    puts ""
+    puts "1) List all Criterion Directors."
+    puts "2) List all Criterion Films."
+    puts "exit) Exit program"
+    input = ""
+    while input != "0"
+      input = gets.strip.downcase
+      case input
       when "1"
         list_directors
       when "2"
         list_films
+      end
     end
+    puts "Goodbye!"
   end
 
-  
-
-  end
-  
-  def list_directors
+  def film_profile
     
+  end
+
+  def list_directors
+    Director.all.each_with_index do |dir, index|
+      puts "#{index + 1}) #{dir.name}"
+    end
   end
   
   def list_films
-    puts "1) 1-500"
-    puts "2) 501-1000"
-    puts "3) 1001-end"
-    input = nil
-    while input != "exit"
-      input = gets.strip.downcase
-      case input
-        when "1"
-          Movie.all.each_with_index do |movie, index|
-            puts "#{index + 1}) #{movie.title}" if index < 500
-          end
-        when "2"
-          Movie.all.each_with_index do |movie, index|
-            puts "#{index + 1}) #{movie.title}" if index >= 500 && index < 501-1000
-          end
-        when "3"
-          Movie.all.each_with_index do |movie, index|
-            puts "#{index + 1}) #{movie.title}" if index > 1000
-          end
-      end
+    Movie.all.each_with_index do |movie, index|
+      puts "#{index + 1}) #{movie.title}"
     end
   end
+  
+  def list_films_by_director
+    
+  end
+  
 end
