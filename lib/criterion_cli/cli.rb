a0b7127734ef44614ac0f33c42e568daa435b7eb
Ethan Sagin <ethansagin@gmail.com>
2019-05-16 20:29:30 +0000

    cli work

--- lib/criterion_cli/cli.rb
+++ lib/criterion_cli/cli.rb
@@ -13,13 +13,14 @@ class CriterionCli::Cli
   def start
     input = nil
     while input != "exit"
-      puts "To browse the list of titles, enter 'list titles'"
-      puts "To exit this program, enter 'exit'"
+      puts ""
+      puts "MAIN MENU"
+      puts "- To browse the list of titles, enter 'list titles'"
+      puts "- To exit this program, enter 'exit'"
       input = gets.strip.downcase
       case input
       when "list titles"
         list_titles
-        view_profile
       when "exit"
         puts "Thank you for using CCC, enjoy the show!"
       else
@@ -32,15 +33,20 @@ class CriterionCli::Cli
   def list_titles
     input = nil
     while input != "menu"
-    puts "Something about inputting numbers"
+    puts ""
+    puts "- To browse the catalog, enter a page number between '1' and '#{(Movie.all.length/100.to_f).ceil}'"
+    puts "- To view a movie profile, enter 'view'"
+    puts "- To return to the previous menu, enter 'menu'"
     input = gets.strip.downcase
       if input == "menu"
-      elsif input.to_i > 0 && input.tp_i <= ((Movie.all.length/100.to_f).ceil)
+      elsif input.to_i > 0 && input.to_i <= ((Movie.all.length/100.to_f).ceil)
         Movie.all.each_with_index do |movie, index|
           if index >= (input.to_i - 1)*100 && index < (input.to_i)*100
             puts "#{index + 1}. #{movie.title}"
           end
         end
+      elsif input == "view"
+        view_profile
       else
         puts "Invalid selection"
         puts ""
@@ -52,7 +58,7 @@ class CriterionCli::Cli
     input = nil
     while input != "menu"
       puts "To view movie profile, enter the movie number"
-      puts "To return to the menu, enter 'menu'"
+      puts "To return to the previous menu, enter 'menu'"
       input = gets.strip.downcase
       if input.to_i > 0 && input.to_i <= Movie.all.length + 1
         movie_profile(input.to_i - 1)
