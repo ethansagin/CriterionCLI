7e4914e5b671f6d312b583db1c76ea1dfafb7814
Ethan Sagin <ethansagin@gmail.com>
2019-05-16 18:22:23 +0000

    added config/env

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+
+require_relative "../lib/criterion_cli/version"
+require_relative "../lib/criterion_cli/movie"
+require_relative "../lib/criterion_cli/scraper"
+require_relative "../lib/criterion_cli/cli"
--- lib/criterion_cli.rb
+++ lib/criterion_cli.rb
@@ -1,8 +1,1 @@
-require 'pry'
-require 'open-uri'
-require 'nokogiri'
-
-require_relative "criterion_cli/version"
-require_relative "criterion_cli/movie"
-require_relative "criterion_cli/scraper"
-require_relative "criterion_cli/cli"
+require_relative "../config/environment"
