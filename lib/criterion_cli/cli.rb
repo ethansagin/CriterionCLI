767e9aa1d1f55516f14edb9dbb29eaf6b348480a
Ethan Sagin <ethansagin@gmail.com>
2019-05-16 20:09:56 +0000

    cli work

--- lib/criterion_cli/cli.rb
+++ lib/criterion_cli/cli.rb
@@ -19,23 +19,55 @@ class CriterionCli::Cli
       case input
       when "list titles"
         list_titles
+        view_profile
+      when "exit"
+        puts "Thank you for using CCC, enjoy the show!"
       else
         puts "Invalid selection"
         puts ""
       end
     end
-    puts "Thank you for using CC, enjoy the show!"
   end
   
   def list_titles
-    Movie.all.each_with_index do |movie, index|
-      puts "#{index + 1}. #{movie.title}"
+    input = nil
+    while input != "menu"
+    puts "Something about inputting numbers"
+    input = gets.strip.downcase
+      if input == "menu"
+      elsif input.to_i > 0 && input.tp_i <= ((Movie.all.length/100.to_f).ceil)
+        Movie.all.each_with_index do |movie, index|
+          if index >= (input.to_i - 1)*100 && index < (input.to_i)*100
+            puts "#{index + 1}. #{movie.title}"
+          end
+        end
+      else
+        puts "Invalid selection"
+        puts ""
+      end
     end
   end
   
-  def movie_profile(movie_index)
-    mov = Movie.all[movie_index]
+  def view_profile
+    input = nil
+    while input != "menu"
+      puts "To view movie profile, enter the movie number"
+      puts "To return to the menu, enter 'menu'"
+      input = gets.strip.downcase
+      if input.to_i > 0 && input.to_i <= Movie.all.length + 1
+        movie_profile(input.to_i - 1)
+      elsif input == "menu"
+      else
+        puts "Invalid selection"
+        puts ""
+      end
+    end
+  end
+
+  def movie_profile(index)
+    mov = Movie.all[index]
     puts "#{mov.title}"
   end
+    
 
 end
