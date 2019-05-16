2b6e6da1864cd21469ab56d9560cbf51982951e4
Ethan Sagin <ethansagin@gmail.com>
2019-05-16 19:12:57 +0000

    cli basic search

--- lib/criterion_cli/cli.rb
+++ lib/criterion_cli/cli.rb
@@ -11,50 +11,32 @@ class CriterionCli::Cli
   end
   
   def start
-    input = ""
-    while input != "exit"
-      puts ""
-      puts "To browse the list of titles, enter 'list titles'"
-      puts "To browse titles by director, enter 'list directors'"
-      puts "To exit this program, enter 'exit'"
-      input = gets.strip.downcase
-      case input
+    puts ""
+    puts "To browse the list of titles, enter 'list titles'"
+    puts "To exit this program, enter 'exit'"
+    input = gets.strip.downcase
+      while input != "exit"
+        case input
         when "list titles"
           list_titles
-        when "list directors"
-          list_directors
-        when "exit"
-          puts "Thank you for using CC, Happy Watching!"
+          start
         else
           puts "Invalid selection"
+          start
       end
     end
+    puts "Thank you for using CC, enjoy the show!"
   end
   
   def list_titles
     Movies.all.each_with_index do |movie, index|
       puts "#{index + 1}. #{movie.name}"
     end
-    
-    input = ""
-    puts "Enter the number of the movie you'd like to see more information about"
-    puts "To return to the main menu, enter '0'"
-    
-    input = gets.strip.to_i
-    while input != 0
-      movie_profile(input - 1)
-    end
-  end
-  
-  def list_directors
-    
   end
   
   def movie_profile(movie_index)
     mov = Movie.all[movie_index]
     puts "#{mov.title}"
   end
-  
-  
 
 end
