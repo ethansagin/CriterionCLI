48b6a779dab2b28456b759d844d305ccb8b4475f
Ethan Sagin <ethansagin@gmail.com>
2019-05-17 21:06:45 +0000

    complete director cli

--- lib/criterion_cli/cli.rb
+++ lib/criterion_cli/cli.rb
@@ -25,6 +25,7 @@ class CriterionCli::Cli
       when "list directors"
         list_directors
       when "exit"
+        ""
         puts "Thank you for using CCC, enjoy the show!"
       else
         puts "Invalid selection"
@@ -39,6 +40,7 @@ class CriterionCli::Cli
     input = nil
     while input != "menu"
     puts ""
+    puts "Movie Titles (Full Catalog) Menu"
     puts "- To browse the catalog, enter a page number between '1' and '#{(Movie.all.length/100.to_f).ceil}'"
     puts "- To view a movie profile, enter 'view'"
     puts "- To return to the previous menu, enter 'menu'"
@@ -47,7 +49,7 @@ class CriterionCli::Cli
       elsif input.to_i > 0 && input.to_i <= ((Movie.all.length/100.to_f).ceil)
         Movie.all.each_with_index do |movie, index|
           if index >= (input.to_i - 1)*100 && index < (input.to_i)*100
-            puts "#{index + 1}. #{movie.title}"
+            puts "#{index + 1}) #{movie.title}"
           end
         end
       elsif input == "view"
@@ -63,6 +65,7 @@ class CriterionCli::Cli
     input = nil
     while input != "menu"
       puts ""
+      puts "Movie Profile Menu"
       puts "- To view movie profile, enter the movie number"
       puts "- To return to the previous menu, enter 'menu'"
       input = gets.strip.downcase
@@ -106,6 +109,7 @@ class CriterionCli::Cli
     input = nil
     while input != "menu"
     puts ""
+    puts "Director List Menu"
     puts "- To browse the director list, enter a page number between '1' and '#{(Director.all.length/100.to_f).ceil}'"
     puts "- For more information on a director, enter 'info'"
     puts "- To return to the previous menu, enter 'menu'"
@@ -114,14 +118,13 @@ class CriterionCli::Cli
       elsif input.to_i > 0 && input.to_i <= ((Director.all.length/100.to_f).ceil)
         Director.all.each_with_index do |dir, index|
           if index >= (input.to_i - 1)*100 && index < (input.to_i)*100
-            puts "#{index + 1}. #{dir.name}"
+            puts "#{index + 1}) #{dir.name}"
           end
         end
       elsif input == "info"
         director_info
       else
         puts "Invalid selection"
-        puts ""
       end
     end
   end
@@ -129,40 +132,70 @@ class CriterionCli::Cli
   def director_info
     input = nil
     while input != "menu"
+      puts ""
+      puts "Director Info Menu"
       puts "- To view more information on a director, enter the director number"
-      puts "- To return to the previous menu, enter 'menu'" 
+      puts "- To view the profile of a movie listed in a director's info, enter 'view'"
+      puts "- To return to the previous menu, enter 'menu'"
+      input = gets.strip.downcase
       if input.to_i > 0 && input.to_i < Director.all.length
         dir = Director.all[input.to_i]
         puts ""
         puts "#{dir.name} Criterion Profile"
         puts "- - - - - - - - - - - - - - - - - "
-        puts "#{dir.name} has directer a total of #{dir.movies.length} in the Criterion Collection:"
-        director_info_mov_list
+        puts "#{dir.name} has directer a total of #{dir.movies.length} films"
+        puts "in the Criterion Collection:"
+        director_info_mov_list(dir)
         puts ""
-        puts "Notable actors/actresses with whom #{dir.name} has collaborated on these films include:"
-#        director_info_cast_collab
-        binding.pry
+        puts "Notable actors/actresses with whom #{dir.name}"
+        puts "has collaborated on these films include:"
+        director_info_cast_collab(dir)
+        puts ""
+        director_info_other_roles(dir)
+        puts ""
+      elsif input == "view"
+        view_profile
+      else
+        puts "Invalid selection"
       end
     end
   end
 
-  def director_info_mov_list
+  def director_info_mov_list(dir)
    dir.movies.each do |moovv|
-      Movies.all.each_with_index do |mov|
+      Movie.all.each_with_index do |mov, index|
         if moovv == mov
           Scraper.movie_info_scraper(mov) unless mov.duration
-          puts "#{index + 1}. #{mov.title}"
+          puts "#{index + 1}) #{mov.title}"
         end
       end
     end
   end
   
-  def director_info_cast_collab
-    
+  def director_info_cast_collab(dir)
+    cast = []
+    dir.movies.each do |mov|
+      cast << mov.cast.keys
+    end
+    cast = cast.flatten.uniq.join(", ")
+    puts "#{cast}"
   end
   
-  def director_info_other_roles
-    
+  def director_info_other_roles(dir)
+    dir_crew = []
+    dir.movies.each do |mov|
+      mov.crew.each do |k, v|
+        if k == dir.name
+          dir_crew << v unless v == "Director"
+        end
+      end
+    end
+    if dir_crew.length > 0 
+      dir_crew = dir_crew.uniq.join(", ")
+      puts "Besides directing, #{dir.name} also contributed to some of"
+      puts "these films in the following roles:"
+      puts "#{dir_crew}"
+    end
   end
 
     
